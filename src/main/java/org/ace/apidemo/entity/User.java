631e90ddb61802f706ed20e5fb4ac62091ab8806
WaiAgPhyo <waiagphyo90@gmail.com>
2023-12-19 09:32:06 +0630

    Initial commit

---
+++ src/main/java/org/ace/apidemo/entity/User.java
@@ -0,0 +1,27 @@
+package org.ace.apidemo.entity;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String address;
+
+}
---
+++ src/main/java/org/ace/apidemo/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package org.ace.apidemo.repo;
+
+import org.ace.apidemo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
+
+
+
+}
